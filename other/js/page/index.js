eeb9523e49d7e20a4ae074a0b5a49847076c3ff5
gaojunle <gaojun_le@163.com>
2018-12-31 20:24:16 +0800

    lazy

---
+++ other/js/page/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by gaojun-pd on 2017/3/23.
+ */
+var App = {
+    init: function () {
+        this.initData();
+        this.bindEvent();
+    },
+    initData: function () {
+
+    },
+
+    bindEvent: function () {
+
+    }
+};
+App.init();
