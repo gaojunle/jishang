b9015618aba253c26c83fbb79cd4ddb38381c068
unknown <gaojun-pd@360.cn>
2020-03-25 18:26:32 +0800

    融创上海

---
+++ 融创/融创上海/js/lib/common.js
@@ -0,0 +1,36 @@
+// true 或false ,返回true的话 则为全面屏
+function judgeBigScreen() {
+    let result = false;
+    const rate = window.screen.height / window.screen.width;
+    let limit = window.screen.height == window.screen.availHeight ? 1.8 : 1.65; // 临界判断值
+
+    // window.screen.height为屏幕高度
+    //  window.screen.availHeight 为浏览器 可用高度
+
+    if (rate > limit) {
+        result = true;
+    }
+    return result;
+};
+(function () {
+    $().ready(function () {
+        if (judgeBigScreen()) {
+            $('.footer').addClass('fullScreen')
+        }
+        flexFull()
+    });
+})();
+
+function flexFull() {
+    var $flexFull = $('.js-flex-full');
+    $flexFull.each(function () {
+        var clen = $(this).children().length;
+        var cols = $(this).data('cols')
+        var bq = clen % cols;
+        for (var i = 0; i < bq; i++) {
+            var $tmp = $('<div class="flex-full-tmp"></div>').width($(this).children().eq(0).width())
+            $(this).append($tmp)
+        }
+    })
+
+}
